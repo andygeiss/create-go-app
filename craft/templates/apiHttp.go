745ef09d877820bb2c288399ce33f1b8c1378b1a
andygeiss <4ndygeiss@gmail.com>
2020-09-30 18:32:40 +0200

    comment fixed

--- craft/templates/apiHttp.go
+++ craft/templates/apiHttp.go
@@ -12,7 +12,7 @@ content-type: application/json
 
 ###
 
-// {{ range $i, $name := .Services }}# {{ lc $name }}
+{{ range $i, $name := .Services }}# {{ lc $name }}
 POST http://localhost:3000/{{ lc $name }} HTTP/1.1
 content-type: application/json
 
