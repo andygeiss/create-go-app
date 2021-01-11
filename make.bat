56a407d9db618d4e58eaa9bd2ac9493bfaf1c2fe
ANDREAS GEIß <Andreas.Geiss@fiduciagad.de>
2021-01-11 13:12:02 +0100

    windows setup updated

--- make.bat
+++ make.bat
@@ -1,6 +1,7 @@
 @echo off
 cls
 setlocal EnableDelayedExpansion
+
 echo getting info from Git ...
 git pull
 git rev-parse --short HEAD > build.txt
@@ -10,8 +11,10 @@ git describe --tags > version.txt
 SET /p VERSION=<version.txt
 echo VERSION = %VERSION%
 echo.
+
 echo start building ...
 go build --ldflags "-s -w -X=main.build=%BUILD% -X=main.name=create-go-app -X=main.version=%VERSION%" -o %GOPATH%\bin\create-go-app.exe main.go
 echo cleaning up ...
 del build.txt version.txt
+
 echo done.
