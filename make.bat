69167ef7bdc88aeaef6ef056c6c2d769b9d11a85
ANDREAS GEIß <Andreas.Geiss@fiduciagad.de>
2021-01-11 10:50:07 +0100

    windows make.bat added

---
+++ make.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+setlocal EnableDelayedExpansion
+echo getting info from Git ...
+git rev-parse --short HEAD > build.txt
+SET /p BUILD=<build.txt
+echo BUILD   = %BUILD%
+git describe --tags > version.txt
+SET /p VERSION=<version.txt
+echo VERSION = %VERSION%
+echo.
+echo start building ...
+go build --ldflags "-s -w -X=main.build=%BUILD% -X=main.name=create-go-app -X=main.version=%VERSION%" -o %GOPATH%\bin\create-go-app.exe main.go
+echo cleaning up ...
+del build.txt version.txt
+echo done.
