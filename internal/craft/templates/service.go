f978657787df3748e4dc5acd00fd08c771bf0362
andygeiss <4ndygeiss@gmail.com>
2021-01-15 14:59:59 +0100

    fix imports

--- internal/craft/templates/service.go
+++ internal/craft/templates/service.go
@@ -5,17 +5,27 @@ var ServiceGo = `package status
 
 import (
 	"context"
+	"encoding/hex"
 
 	"{{ .Path }}/pkg/api"
 	"{{ .Path }}/pkg/repository"
+	"{{ .Path }}/pkg/secure"
 )
 
 // Service ...
 func Service(repo repository.Repository) api.StatusService {
+	// Save a plaintext value
 	repo.Add("status", "OK")
+	// Save an encrypted password
+	secret := hex.EncodeToString(secure.HashPassword([]byte("password")))
+	repo.Add("secret", secret)
 	return func(ctx context.Context, req *api.StatusRequest) (res *api.StatusResponse, err error) {
-		return &api.StatusResponse{
+		// Create a response
+		response := &api.StatusResponse{
+			// Only read the plaintext value as an example
 			Text: repo.FindByID("status").(string),
-		}, nil
+		}
+		return response, nil
 	}
-}`
+}
+`
